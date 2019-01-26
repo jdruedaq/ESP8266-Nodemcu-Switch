23d73e609a3932242a1048f603dd602119cdf1d1
juandrueda <juanptte@gmail.com>
2019-01-26 18:50:16 -0500

    Added touch sensor switch function

--- ControlPorVoz.ino
+++ ControlPorVoz.ino
@@ -8,15 +8,17 @@
 #define WIFI_SSID "Bucuru_Rueda"  
 #define WIFI_PASSWORD "cga010119216801"
 #define LED 2
+#define TOUCH_SWITCH 4
 #define WIFI_CONNECTED 12
 
 //0 y 1 invertido
 
 void setup() {
-  pinMode(LED,OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(TOUCH_SWITCH, INPUT);
   pinMode(WIFI_CONNECTED, OUTPUT);
   digitalWrite(LED,1);
-  Serial.begin(9600);
+  Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -34,20 +36,38 @@ void setup() {
 }
 
 void loop() {
+
   if(Firebase.getString("LEDstatus").toInt()) {
     digitalWrite(LED,0);
   } else {
     digitalWrite(LED,1);
   }
-
+  
+  Serial.print("LED -> ");
+  Serial.println(digitalRead(LED));
+  Serial.print("TOUCH -> ");
+  Serial.println(digitalRead(TOUCH_SWITCH));
+   
+  if(digitalRead(TOUCH_SWITCH) == HIGH) {
+    if(digitalRead(LED) == HIGH) {
+      Firebase.setString("LEDstatus", "1");
+      digitalWrite(LED,0);
+    } else {
+      Firebase.setString("LEDstatus", "0");
+      digitalWrite(LED,1);
+    }
+    delay(500);
+  }
+ 
+  Serial.print("FirebaseStatus -> ");
   Serial.println(Firebase.getString("LEDstatus").toInt());
 
   if (Firebase.failed()) {
     Serial.print("setting /number failed:");
     Serial.println(Firebase.error());
     digitalWrite(WIFI_CONNECTED, LOW);
-    ESP.reset();
+    //setup();
     return;
   }
-  delay(1000);
+  //delay(500);
 }
